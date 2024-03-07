98b29f035a6fa41851e9806eaede144d9835d91f
davidwong <arkwanyuk@gmail.com>
2024-03-07 19:48:15 +0800

    Mode data processing module into locator package

--- fruit_locator/data_processing.py
+++ fruit_locator/data_processing.py
@@ -1,14 +1,19 @@
-import argparse
 import os
 import numpy as np
 import cv2
 
 
-def main(args):
+def numpy_to_image(input_path: str, output_path: str) -> None:
+    """
+    Convert the given .npy files to .png images
+    :param input_path: Path to input numpy file or folder
+    :param output_path: Path to output images
+    :return: None
+    """
 
-    input_path = args.input
     assert os.path.exists(input_path), f"Input path not found at {input_path}"
 
+    # Determine list of input .npy files
     input_files = []
     if os.path.isfile(input_path):
         input_files.append(input_path)
@@ -19,9 +24,9 @@ def main(args):
             if file.endswith(".npy")
         ]
 
-    output_folder = args.output
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder, exist_ok=True)
+    # Create the output folder if it does not exist
+    if not os.path.exists(output_path):
+        os.makedirs(output_path, exist_ok=True)
 
     # Loop through each input file
     for input_file in input_files:
@@ -32,28 +37,8 @@ def main(args):
         filename = os.path.splitext(os.path.basename(input_file))[0]
 
         # Save the image as a PNG file
-        output_file = os.path.join(output_folder, f"{filename}.png")
+        output_file = os.path.join(output_path, f"{filename}.png")
 
         print(f"Convert {input_file} to {output_file}")
 
         cv2.imwrite(output_file, image)
-    pass
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Convert numpy to images")
-    parser.add_argument(
-        "--input",
-        type=str,
-        default="/mnt/hdd/davidwong/data/FruitDataset/dataset/input_numpy",
-        help="Path to input numpy file or folder",
-    )
-    parser.add_argument(
-        "--output",
-        type=str,
-        default="/mnt/hdd/davidwong/data/FruitDataset/dataset/input_images",
-        help="Path to output images",
-    )
-
-    args = parser.parse_args()
-    main(args)
